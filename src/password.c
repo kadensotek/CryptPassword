2d28903b5be5665d45a90aab1dc9c9978274effd
kadensotek <bobby23jones@gmail.com>
2013-04-10 23:09:05 -0400

    Added some function protypes that I'll need

--- src/password.c
+++ src/password.c
@@ -5,6 +5,11 @@
 #include <time.h>
 #include <crypt.h>
 
+void printMainMenu();
+void addUser(char *filename);
+void deleteUser(char *filename);
+void authenticate(char *filename);
+
 int main(int argc, char *argv[])
 {
     printf("Hello, world!\n");
